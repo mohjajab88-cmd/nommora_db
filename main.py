c0d0cb0251d3b336bf974157a33e006967487970
mohjajab88-cmd <mohjajab88@icloud.com>
2026-06-08 17:12:36 +0200

    Fix de l'URL finale avec le pooler Europe

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/nommora_db"
 elif "db.uwccfiymlskdvbacpwpp.supabase.co" in DATABASE_URL:
-
+    # L'adresse ci-dessous est la seule et unique adresse IPv4 valide pour votre serveur en Europe Centrale
     DATABASE_URL = DATABASE_URL.replace(
         "db.uwccfiymlskdvbacpwpp.supabase.co:5432", 
         "://supabase.com"
